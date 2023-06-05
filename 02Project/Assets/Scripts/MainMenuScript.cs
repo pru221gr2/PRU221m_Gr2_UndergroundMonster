240aad95afb042312132341f102f43061d95ace2
congthinh.161 <congthinh.work161@gmail.com>
2023-06-06 05:45:25 +0700

    Fix load main menu , add scoreboard

--- 02Project/Assets/Scripts/MainMenuScript.cs
+++ 02Project/Assets/Scripts/MainMenuScript.cs
@@ -10,6 +10,7 @@ public class MainMenuScript : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1.0f;
     }
     public void QuitGame()
     {
