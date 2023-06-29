aa032b5b883c2117cecea62227ed49a3fb1c1b3b
tho dz <thondhe150527@fpt.edu.vn>
2023-06-29 10:13:53 +0700

    buy turret

--- 02Project/Assets/Scripts/Tower/ClickToBaseTower.cs
+++ 02Project/Assets/Scripts/Tower/ClickToBaseTower.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ClickToBaseTower : MonoBehaviour
@@ -7,6 +8,37 @@ public class ClickToBaseTower : MonoBehaviour
     List<Object> listTower;
     static Transform transformTower;
     static List<Transform> listTransformTowerUsed = new List<Transform>();
+    TextMeshProUGUI turrentOneLevelOne;
+    TextMeshProUGUI turrentOneLevelTwo;
+    TextMeshProUGUI turrentOneLevelThree;
+
+    TextMeshProUGUI turrentTwoLevelOne;
+    TextMeshProUGUI turrentTwoLevelTwo;
+    TextMeshProUGUI turrentTwoLevelThree;
+
+    TextMeshProUGUI turrentThreeLevelOne;
+    TextMeshProUGUI turrentThreeLevelTwo;
+    TextMeshProUGUI turrentThreeLevelThree;
+
+    Canvas towerOption;
+    private void Start()
+    {
+        turrentOneLevelOne = GameObject.FindGameObjectWithTag("PriceOfTower1Level1").GetComponent<TextMeshProUGUI>();
+        //turrentOneLevelTwo = GameObject.FindGameObjectWithTag("PriceOfTower1Level2").GetComponent<TextMeshProUGUI>();
+        //turrentOneLevelThree = GameObject.FindGameObjectWithTag("PriceOfTower1Level3").GetComponent<TextMeshProUGUI>();
+
+        turrentTwoLevelOne = GameObject.FindGameObjectWithTag("PriceOfTower2Level1").GetComponent<TextMeshProUGUI>();
+        //turrentTwoLevelTwo = GameObject.FindGameObjectWithTag("PriceOfTower2Level2").GetComponent<TextMeshProUGUI>();
+        //turrentTwoLevelThree = GameObject.FindGameObjectWithTag("PriceOfTower2Level3").GetComponent<TextMeshProUGUI>();
+
+        turrentThreeLevelOne = GameObject.FindGameObjectWithTag("PriceOfTower3Level1").GetComponent<TextMeshProUGUI>();
+        //turrentThreeLevelTwo = GameObject.FindGameObjectWithTag("PriceOfTower3Level2").GetComponent<TextMeshProUGUI>();
+        //turrentThreeLevelThree = GameObject.FindGameObjectWithTag("PriceOfTower3Level3").GetComponent<TextMeshProUGUI>();
+
+        turrentOneLevelOne.text = Collect.MoneyTurretOneLevelOne+"$";
+        turrentTwoLevelOne.text = Collect.MoneyTurretTwoLevelOne+"$";
+        turrentThreeLevelOne.text = Collect.MoneyTurretThreeLevelOne+"$";
+    }
     //when player click to base tower
     private void OnMouseDown()
     {
@@ -16,6 +48,7 @@ public class ClickToBaseTower : MonoBehaviour
         if (listTransformTowerUsed.Contains(transformTower))
         {
             //Update tower will be create in phase two
+            Debug.Log("con cec");
         }
         else
         {
@@ -34,31 +67,58 @@ public class ClickToBaseTower : MonoBehaviour
     //When player chosse Tower 1
     public void ClickTowerButtonOne()
     {
+        GameObject.FindGameObjectWithTag("PriceOfTower1Level1").GetComponent<TextMeshProUGUI>().color = Color.black;
         //add build audio
+        if (Collect.countCoin > Collect.MoneyTurretOneLevelOne)
+        {
         AudioManager.Instance.PlaySFX("Build");
-        Debug.Log("Db");
+        Collect.countCoin-=Collect.MoneyTurretOneLevelOne;
         listTransformTowerUsed.Add(transformTower);
         Instantiate(listTower[0], transformTower.position, Quaternion.identity);
-        GameObject.FindGameObjectWithTag("TowerOption").GetComponent<Canvas>().enabled = false;
+            GameObject.FindGameObjectWithTag("TowerOption").GetComponent<Canvas>().enabled = false;
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("PriceOfTower1Level1").GetComponent<TextMeshProUGUI>().color = Color.red;
+        }
+        
     }
     //When player chosse Tower 2
     public void ClickTowerButtonTwo()
     {
+        GameObject.FindGameObjectWithTag("PriceOfTower2Level1").GetComponent<TextMeshProUGUI>().color = Color.black;
         //add build audio
-        AudioManager.Instance.PlaySFX("Build");
-
-        listTransformTowerUsed.Add(transformTower);
-        Instantiate(listTower[1], transformTower.position, Quaternion.identity);
-        GameObject.FindGameObjectWithTag("TowerOption").GetComponent<Canvas>().enabled = false;
+        if (Collect.countCoin > Collect.MoneyTurretTwoLevelOne)
+        {
+            //add build audio
+            AudioManager.Instance.PlaySFX("Build");
+            Collect.countCoin -= Collect.MoneyTurretTwoLevelOne;
+            listTransformTowerUsed.Add(transformTower);
+            Instantiate(listTower[1], transformTower.position, Quaternion.identity);
+            GameObject.FindGameObjectWithTag("TowerOption").GetComponent<Canvas>().enabled = false;
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("PriceOfTower2Level1").GetComponent<TextMeshProUGUI>().color = Color.red;
+        }
     }
     //When player chosse Tower 3
     public void ClickTowerButtonThree()
     {
+        GameObject.FindGameObjectWithTag("PriceOfTower3Level1").GetComponent<TextMeshProUGUI>().color = Color.black;
         //add build audio
-        AudioManager.Instance.PlaySFX("Build");
+        if (Collect.countCoin > Collect.MoneyTurretThreeLevelOne)
+        {
+            AudioManager.Instance.PlaySFX("Build");
+            Collect.countCoin -= Collect.MoneyTurretThreeLevelOne;
+            listTransformTowerUsed.Add(transformTower);
+            Instantiate(listTower[2], transformTower.position, Quaternion.identity);
+            GameObject.FindGameObjectWithTag("TowerOption").GetComponent<Canvas>().enabled = false;
 
-        listTransformTowerUsed.Add(transformTower);
-        Instantiate(listTower[2], transformTower.position, Quaternion.identity);
-        GameObject.FindGameObjectWithTag("TowerOption").GetComponent<Canvas>().enabled = false;
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("PriceOfTower3Level1").GetComponent<TextMeshProUGUI>().color = Color.red;
+        }
     }
 }
